d56f47e135a7d790cb923b26a802b57921c434f7
CHRISTIAN YOUNG <CHRISTIAN.YOUNG@OSPRI.CO.NZ>
2024-01-29 12:50:28 +1300

    finishiing touches

--- contentfeed/static/JS/searchToggle.js
+++ contentfeed/static/JS/searchToggle.js
@@ -1,20 +1,17 @@
 function searchToggle() {
     var x = document.getElementById("searchbar");
+    var y = document.getElementById("sbuttS");
+    var z = document.getElementById("sbuttX");
     if(x.style.display !== "block" )
     {
         x.style.display = "block"
+        y.style.visibility = "hidden"
+        z.style.visibility = "visible"
     }
     else
     {
         x.style.display = "none"
+        y.style.visibility = "visible"
+        z.style.visibility = "hidden"
     }
-    
-    // if(x.style.display !== "none" )
-    // {
-    //     x.style.display = "none"
-    // }
-    // else
-    // {
-    //     x.style.display = "block"
-    // }
 };
